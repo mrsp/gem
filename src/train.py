8b9bea9ddf5f4da60bfc96244fc539e5b68e66a0
mrsp <stpiperakis@gmail.com>
2018-10-08 15:17:55 +0300

    added train functionality

--- src/train.py
+++ src/train.py
@@ -7,52 +7,59 @@ from sklearn.metrics import confusion_matrix
 import numpy as np
 import cPickle as pickle
 import matplotlib.pyplot as plt
-
-gt = GeM_tools(comp_filtering=True, freq=500, a=0.995, gt_comparison=True, mu=1.0)
-brf = np.zeros(3)
-brt = np.zeros(3)
-blf = np.zeros(3)
-blt = np.zeros(3)
-blf[2] = -19.1734
-brf[2] = -17.1439
+import sys
 
 
 
 
-gt.input_data('/home/master/Dropbox/nao_contact/test_bag/cross1_test4/',blf,blt,brf,brt)
-#gt.input_data('/Users/ownage/Dropbox/nao_contact/test_bag/cross/cross1/')
-#gt.input_data('/Users/ownage/Dropbox/nao_contact/test_bag/test7/Rot/')
-
-g = GeM()
-data_train = gt.data_train
-g.fit(data_train, 'pca', 'gmm')
+if __name__ == "__main__":
 
 
+	path = str(sys.argv[1])
+	gt = GeM_tools(comp_filtering=True, freq=500, a=0.995, gt_comparison=True, mu=1.0)
+	brf = np.zeros(3)
+	brt = np.zeros(3)
+	blf = np.zeros(3)
+	blt = np.zeros(3)
+	blf[2] = -19.1734
+	brf[2] = -17.1439
 
 
+	if path == " ":
+		path ="/home/user/GEM_test_valkyrie/"
+	
+	gt.input_data(path,blf,blt,brf,brt)
 
-gt.genGroundTruthStatistics(g.reduced_data_train)
-gt.plot_results(g.reduced_data_train, gt.phase, gt.mean, gt.covariance, 'Ground-Truth Labels')
-
-
-gt.plot_results(g.reduced_data_train, g.predicted_labels_train, g.kmeans.cluster_centers_, None, 'Clustering with K-means')
-
-
-gt.plot_results(g.reduced_data_train, g.predicted_labels_train, g.gmm.means_, g.gmm.covariances_, 'Clustering with Gaussian Mixture Models')
-
-
-gt.plot_latent_space(g)
-cnf_matrix = confusion_matrix(gt.phase,  g.predicted_labels_train)
-np.set_printoptions(precision=2)
-class_names = ['DS','LSS','RSS']
-gt.plot_confusion_matrix(cnf_matrix, class_names, 'GMM Confusion matrix')
+	g = GeM()
+	data_train = gt.data_train
+	g.fit(data_train, 'pca', 'gmm')
 
 
 
-pickle.dump(g, open('gem_train.save','wb'))
-pickle.dump(gt, open('gem_train_tools.save','wb'))
 
-print('Training Finished')
+
+	gt.genGroundTruthStatistics(g.reduced_data_train)
+	gt.plot_results(g.reduced_data_train, gt.phase, gt.mean, gt.covariance, 'Ground-Truth Labels')
+
+
+	gt.plot_results(g.reduced_data_train, g.predicted_labels_train, g.kmeans.cluster_centers_, None, 'Clustering with K-means')
+
+
+	gt.plot_results(g.reduced_data_train, g.predicted_labels_train, g.gmm.means_, g.gmm.covariances_, 'Clustering with Gaussian Mixture Models')
+
+
+	gt.plot_latent_space(g)
+	cnf_matrix = confusion_matrix(gt.phase,  g.predicted_labels_train)
+	np.set_printoptions(precision=2)
+	class_names = ['DS','LSS','RSS']
+	gt.plot_confusion_matrix(cnf_matrix, class_names, 'GMM Confusion matrix')
+
+
+
+	pickle.dump(g, open('gem_train.save','wb'))
+	pickle.dump(gt, open('gem_train_tools.save','wb'))
+
+	print('Training Finished')
 
 
 
